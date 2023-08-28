e8025dd19dd62971815e6de28e9dd94f0da3225a
Sümeyra Kılıçoğlu <119740024+Smyakilicoglu@users.noreply.github.com>
2023-08-28 11:52:00 +0300

    Add files via upload

---
+++ sifreleme/sezar_sifresi.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  3 17:24:09 2023
+
+@author: Sümeyra KILIÇOĞLU
+"""
+#Şifrelerle dolu Sezar alfabesi
+#alfabe durmak istenilen sayıda harfleri kaydırıyo.
+
+alfabe = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+kelime = input("Buraya bir kelime giriniz: ")
+liste = list(kelime)
+sayi = input("Alfabenin ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
